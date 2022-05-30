e20d54599577a6835235a791c93f021df89fa19f
Developer <developer@pop-os.localdomain>
2022-05-30 17:12:39 +0300

    tests if loading works properly

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,69 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import { REPOSITORIES_QUERY } from "./query";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const NUMBER_RESULT_PER_PAGE = 10;
+
+const mocks = (hasNext: boolean = true, hasPrevious: boolean = true) => [
+  {
+    request: {
+      query: REPOSITORIES_QUERY,
+      variables: {
+        before: null,
+        after: null,
+        first: NUMBER_RESULT_PER_PAGE,
+        last: null,
+      },
+    },
+    result: {
+      data: {
+        search: {
+          pageInfo: {
+            endCursor: "Y3Vyc29yOjIw",
+            hasNextPage: hasNext,
+            hasPreviousPage: hasPrevious,
+            startCursor: "Y3Vyc29yOjEx",
+            __typename: "PageInfo",
+          },
+          nodes: [
+            {
+              description:
+                "A declarative, efficient, and flexible JavaScript library for building user interfaces.",
+              forkCount: 38904,
+              id: "MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==",
+              name: "react",
+              stargazerCount: 188755,
+              __typename: "Repository",
+            },
+          ],
+          repositoryCount: 2951568,
+          __typename: "SearchResultItemConnection",
+        },
+      },
+    },
+  },
+];
+
+describe("Github app tests", () => {
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      value: () => {
+        return {
+          matches: true,
+          addListener: jest.fn(),
+          removeListener: jest.fn(),
+        };
+      },
+    });
+  });
+
+  test("renders loading list", () => {
+    render(
+      <MockedProvider mocks={mocks()}>
+        <App />
+      </MockedProvider>
+    );
+    expect(screen.getByText("Loading..."));
+  });
 });
