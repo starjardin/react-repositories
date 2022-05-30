041ecd03e1b82171f7d39851c1c04b8883d7767d
Developer <developer@pop-os.localdomain>
2022-05-30 17:23:53 +0300

    displays react repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TableView, { DataType } from "./Table";
+import { useQuery } from "@apollo/client";
+import { REPOSITORIES_QUERY } from "./query";
+import React, { useCallback } from "react";
+import { QueryVariables, RepositoryResult } from "./interfaces";
+import { Button, ButtonContainer, Loading, Wrapper } from "./styles";
+
+const NUMBER_RESULT_PER_PAGE = 10;
 
 function App() {
+  const { data, loading, error, refetch } = useQuery<
+    RepositoryResult,
+    QueryVariables
+  >(REPOSITORIES_QUERY, {
+    variables: {
+      before: null,
+      after: null,
+      first: NUMBER_RESULT_PER_PAGE,
+      last: null,
+    },
+  });
+
+  const backDisabled = !data?.search.pageInfo.hasPreviousPage;
+  const nextDisabled = !data?.search.pageInfo.hasNextPage;
+
+  const handleNextPage = useCallback(() => {
+    const endCursor = data?.search.pageInfo.endCursor;
+    refetch({ after: endCursor });
+  }, [data?.search.pageInfo]);
+
+  const handlePreviousPage = useCallback(() => {
+    const startCursor = data?.search.pageInfo.startCursor;
+    refetch({ before: startCursor, last: NUMBER_RESULT_PER_PAGE, first: null });
+  }, [data?.search.pageInfo]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      {loading && <Loading>Loading...</Loading>}
+      {data && (
+        <React.Fragment>
+          <TableView data={data?.search.nodes as DataType[]} />
+          <ButtonContainer>
+            <Button onClick={handlePreviousPage} disabled={backDisabled}>
+              Back
+            </Button>
+            <Button onClick={handleNextPage} disabled={nextDisabled}>
+              Next
+            </Button>
+          </ButtonContainer>
+        </React.Fragment>
+      )}
+      {error && <div>There was an error while fetching data</div>}
+    </Wrapper>
   );
 }
 
